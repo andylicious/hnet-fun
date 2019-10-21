b30d167a725e9ba6f2ce5cec748c788894cb3f92
Andreas Mann <andreasmann94@gmail.com>
2019-10-21 21:47:27 +0200

    Added a cron job to test out cron func

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,48 @@
 require('dotenv').config();
+const CronJob = require('cron').CronJob;
 import filterArrays from './utils/filter';
 import fetchData from './fetchData';
-import CondoModel, { ICondoModel, postMany } from './models/condo';
+import CondoModel, { postMany } from './models/condo';
 import mongoose from 'mongoose';
 
+// TODO: LOGGER
 const main = async () => {
   // console.log('First call');
-  try {
-    await mongoose.connect(
-      `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CONNECTION_STRING}/${process.env.MONGO_DB_DATABASE}`,
-      { useNewUrlParser: true }
-    );
-    console.log('Connected');
-  } catch (e) {
-    console.log('Failed connecting');
-  }
+  // TODO: Refactor this and cron job to own setup function
+  // try {
+  //   await mongoose.connect(
+  //     `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CONNECTION_STRING}/${process.env.MONGO_DB_DATABASE}`,
+  //     { useNewUrlParser: true }
+  //   );
+  //   console.log('Connected');
+  // } catch (e) {
+  //   console.log('Failed connecting');
+  // }
 
-  const existingRecords = CondoModel.find({});
-  const newRecords = fetchData();
-  const records = await Promise.all([existingRecords, newRecords]);
+  console.log('Before job instantiation');
+  const job = new CronJob('*/5 * * * * *', function() {
+    console.log('Job running');
+  });
+  console.log('After job instantiation');
+  job.start();
 
-  const saveRecords = filterArrays(
-    records[1],
-    records[0].map((item: any) => item.id)
-  );
+  // TODO: Refactor to its own function
+  // const existingRecords = CondoModel.find({});
+  // const newRecords = fetchData();
+  // const records = await Promise.all([existingRecords, newRecords]);
 
-  try {
-    await postMany(saveRecords);
+  // const saveRecords = filterArrays(
+  //   records[1],
+  //   records[0].map((item: any) => item.id)
+  // );
 
-    console.log('Posted items');
-  } catch (e) {
-    console.log('Faile posting to DB');
-  }
+  // try {
+  //   await postMany(saveRecords);
+
+  //   console.log('Posted items');
+  // } catch (e) {
+  //   console.log('Faile posting to DB');
+  // }
 
   // console.log('Second call');
 };
